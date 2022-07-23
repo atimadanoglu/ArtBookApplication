4b62fd1c6a5a279e691241b4f23c62dae9abc526
Atakan Madanoğlu <ati64.98.35@gmail.com>
2022-07-23 22:59:12 +0300

    Implemented test cases for viewModel

---
+++ app/src/test/java/com/atakanmadanoglu/artbookapplication/repo/FakeArtRepository.kt
@@ -0,0 +1,33 @@
+package com.atakanmadanoglu.artbookapplication.repo
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.atakanmadanoglu.artbookapplication.model.ImageResponse
+import com.atakanmadanoglu.artbookapplication.roomdb.Art
+import com.atakanmadanoglu.artbookapplication.util.Resource
+
+class FakeArtRepository: ArtRepositoryInterface {
+
+    private val arts = mutableListOf<Art>()
+    private val artsLiveData = MutableLiveData<List<Art>>(arts)
+
+    override suspend fun insertArt(art: Art) {
+        arts.add(art)
+    }
+
+    override suspend fun deleteArt(art: Art) {
+        arts.remove(art)
+    }
+
+    override suspend fun searchImage(imageString: String): Resource<ImageResponse> {
+        return Resource.success(ImageResponse(listOf(), 0, 0))
+    }
+
+    override fun getArt(): LiveData<List<Art>> {
+        return artsLiveData
+    }
+
+    fun refreshData() {
+        artsLiveData.postValue(arts)
+    }
+}
